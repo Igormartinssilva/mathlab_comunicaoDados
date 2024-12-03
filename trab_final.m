4c6c376754eeddf42606bffcfacb380bd65a4655
Artur <arturruizs@gmail.com>
2024-12-03 14:13:27 -0300

    Adicionei meu nome e mais umas variaveis, também coloquei o código de exemplo do prof pra termos uma referencia
    Signed-off-by: Artur <arturruizs@gmail.com>

--- trab_final.m
+++ trab_final.m
@@ -1,5 +1,5 @@
 %TRABALHO FINAL
-%GRUPO 4 - IGOR MARTINS SILVA (00333069) E ARTHUR ALGUMA COISA
+%GRUPO 4 - IGOR MARTINS SILVA (00333069) E ARTUR RUIZ DE SOUZA (00334954)
 close all;
 clear;
 
@@ -10,7 +10,13 @@ n = 1296;
 R = 2/3;
 frame_size = 2300 * 8;
 Eb_N0_dB = -2:1:12; % Faixa de Eb/N0 em dB
+Eb_N0_lin = 10 .^ (Eb_N0_dB / 10); % Faixa de Eb/N0 em linearizada
 num_frames = 100; % Número de quadros simulados por Eb/N0
+
+
+NP = Eb ./ (Eb_N0_lin); %vetor de potências do ruído
+NA = sqrt(NP); %vetor de amplitudes do ruído
+
 
 %declaração das matrizes e vetores
 ber_qpsk = zeros(3, length(Eb_N0_dB));
