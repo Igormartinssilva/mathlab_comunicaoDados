d1105d9c6a80860239914e8c187045e0293be56d
Artur <arturruizs@gmail.com>
2024-12-11 18:33:34 -0300

    metodos ldpc funcionando, porem ainda dão resultados duvidosos
    Signed-off-by: Artur <arturruizs@gmail.com>

--- trab_final.m
+++ trab_final.m
@@ -6,7 +6,7 @@ clc;
 %declaração de variáveis
 mod_QPSK = 4; 
 mod_QAM = 64;
-n = 1296; %comprimento do código
+n = 64800; %comprimento do código
 num_b = n; 
 R = 2/3;
 k = R*n; %bits de mensagem
@@ -16,9 +16,9 @@ Eb_N0_dB = -2:1:12; % Faixa de Eb/N0 em dB
 Eb_N0_lin = 10 .^ (Eb_N0_dB / 10); % Faixa de Eb/N0 em linearizada
 num_frames = 100; % Número de quadros simulados por Eb/N0
 
-mensagem = logical(randi(2,1,k)-1);
-mensagemDemod = zeros(1, k);
-mensagemDemodDecod = zeros(1, k);
+mensagem = randi(2,k,1)-1;
+mensagemDemod = zeros(k, 1);
+mensagemDemodDecod = zeros(k, 1);
 
 %declaração das matrizes e vetores
 ber_qpsk = zeros(3, length(Eb_N0_lin));
@@ -34,33 +34,41 @@ P = randi([0 1], bits_pari, k); % Parte aleatória (paridade)
 I = eye(bits_pari);             % Matriz identidade
 H = [P I];              % Combinação de P e I
 
-ldpcEncoder = comm.LDPCEncoder(dvbs2ldpc(R));
-ldpcDecoder = comm.LDPCDecoder('ParityCheckMatrix',dvbs2ldpc(R), 'DecisionMethod','Hard decision');
+
 %Variáveis
 Eb = sqrt(2); % Energia média para quadratura
 NP = Eb ./ (Eb_N0_lin); %vetor de potências do ruído
 NA = sqrt(NP); %vetor de amplitudes do ruído
+ldpcEncoder = comm.LDPCEncoder(dvbs2ldpc(R));
+ldpcDecoder = comm.LDPCDecoder('ParityCheckMatrix',dvbs2ldpc(R), 'DecisionMethod','Hard decision');
+qpskmod = comm.QPSKModulator;
+qpskdemodHard = comm.QPSKDemodulator;
+qpskdemodSoft = comm.QPSKDemodulator('DecisionMethod', 'Log-likelihood ratio');
 
-mensagemCod = step(ldpcEncoder, mensagem);
-SModCod = manual_qpskmod(mensagemCod); 
-SMod = manual_qpskmod(mensagem); %Sinal resultante, modulado e complexo
+mensagemCod = ldpcEncoder.step(mensagem);
+SModCod = qpskmod.step(mensagemCod); 
+SMod = qpskmod.step(mensagem); %Sinal resultante, modulado e complexo
 
 for i = 1:length(Eb_N0_lin)
-    N = NA(i)*complex(randn(1, k/2), randn(1, k/2))*sqrt(0.5); %vetor de ruído complexo com desvio padrão igual a uma posição do vetor NA
-    r = SMod + N ; % vetor recebido
-    mensagemDemod = manual_qpskdemod(r);
-    mensagemDemodDecod = ldpcDecoder(manual_qpskdemod(r));
-    length(mensagem)
-    length(mensagemDemod)
+    NSemCod = NA(i)*complex(randn(length(SMod), 1), randn(length(SMod), 1))*sqrt(0.5); %vetor de ruído complexo com desvio padrão igual a uma posição do vetor NA
+    NCod = NA(i)*complex(randn(length(SModCod), 1), randn(length(SModCod), 1))*sqrt(0.5);
+    rSemCod = SMod + NSemCod; % vetor recebido
+    rCod = SModCod + NCod;
+    
+    mensagemDemod = qpskdemodHard.step(rSemCod);
+    mensagemDemodDecodHard = ldpcDecoder.step(qpskdemodHard.step(rCod));
+    mensagemDemodDecodSoft = ldpcDecoder.step(qpskdemodSoft.step(rCod));
+    
     ber_qpsk(1, i) = sum(mensagem ~= mensagemDemod) / k; % contagem de erros e cálculo do BER para QPSK sem codificação
-    ber_qpsk(2, i) = sum(mensagem ~= mensagemDemodDecod) / k; % contagem de erros e cálculo do BER QPSK com codificação Hard
+    ber_qpsk(2, i) = sum(mensagem ~= mensagemDemodDecodHard) / k; % contagem de erros e cálculo do BER QPSK com codificação Hard
+    ber_qpsk(3, i) = sum(mensagem ~= mensagemDemodDecodSoft) / k; % contagem de erros e cálculo do BER QPSK com codificação Soft
 end
 
 figure(1);
-semilogy(Eb_N0_dB, ber_qpsk(1,:), 'r', 'LineWidth', 2, 'MarkerSize', 10, Eb_N0_dB, ber_qpsk(2,:), 'g', 'LineWidth', 2, 'MarkerSize', 10);
+semilogy(Eb_N0_dB, ber_qpsk(1,:), 'r', Eb_N0_dB, ber_qpsk(2,:), 'g', Eb_N0_dB, ber_qpsk(3,:), 'b');
 xlabel('Eb/N0 (dB)');
 ylabel('BER');
-legend('QPSK sem Cod');
+legend('QPSK sem Cod', 'QPSK LDPC Hard', 'QPSK LDPC Soft');
 
 %--------------------------------------LDPC encoder------------------------
 
